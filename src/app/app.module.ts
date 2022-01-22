fba097a611419418ab1d44acd2c9f219bd361a86
ayortedra <cesaraugusto.pacheco@estudiantes.unahur.edu.ar>
2022-01-22 18:54:33 -0300

    Primer botton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BottonComponent } from './components/botton/botton.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BottonComponent
   ],
   imports: [
     BrowserModule
