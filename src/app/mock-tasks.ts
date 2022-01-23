4a9a7e63f3a57579c729bf7a50e105530b32716e
ayortedra <cesaraugusto.pacheco@estudiantes.unahur.edu.ar>
2022-01-23 10:14:04 -0300

    Agregar mock de las tares y la interface de la mismas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[]= [
+    {
+        id:1,
+        text: "Terminar Angular",
+        day:"Enero 28 a las 18hs",
+        reminder: true
+    },
+    {
+        id:2,
+        text: "Estudiar matematica",
+        day:"Enero 24 a las 14hs",
+        reminder: true
+    },
+    {
+        id:3,
+        text: "Estudiar Introduccion a la programacion",
+        day:"Enero 25 a las 14hs",
+        reminder: true
+    },
+    {
+        id:4,
+        text: "Estudiar Estructura de datos",
+        day:"Enero 26 a las 14hs",
+        reminder: false
+    }
+] 
