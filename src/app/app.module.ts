0b445589b29051a241a80bbe02e6b17f526dae3d
ayortedra <cesaraugusto.pacheco@estudiantes.unahur.edu.ar>
2022-01-23 12:00:56 -0300

    lista de item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BottonComponent } from './components/botton/botton.component';
+import { TaskComponent } from './components/tasks/task.component';
+import { TaskItemComponent } from './components/tasks-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BottonComponent
+    BottonComponent,
+    TaskComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
